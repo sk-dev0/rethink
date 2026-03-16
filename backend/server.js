d24a019d8198c9f47362826bbb3ee74d2cb52d5d
sk-dev0 <learnnote0000@gmail.com>
2026-03-16 12:37:18 +0900

    socket.ioをインストールして接続ができるようにした。

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const engine = require('ejs-mate');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+const http = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.engine('ejs', engine);
 app.set('views', path.join(__dirname, 'views'));
@@ -10,7 +15,8 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.send('テストホームページ');
+    const roomId = uuidv4();
+    res.render('index', { roomId });
 });
 
 app.get('/index', (req, res) => {
@@ -21,6 +27,14 @@ app.use((req, res) => {
     res.send('404ページ');
 });
 
-app.listen(3000, (req, res) => {
+io.on('connection', (socket) => {
+    console.log('接続されました:', socket.id);
+
+    socket.on('disconnect', () => {
+        console.log('切断されました:', socket.id);
+    });
+});
+
+server.listen(3000, () => {
     console.log('ポート3000でリクエスト待ち受け中...');
 });
